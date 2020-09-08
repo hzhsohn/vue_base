057790ef79febf5ef121f2b1bd153f8947ebd0e9
han.zh <han.zhihong@qq.com>
2020-09-09 02:07:11 +0800

    first commit

---
+++ src/components/ABC.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="hello">
+    ABC很<h2>靓仔</h2>{{msg}}
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: '人生如鸡肠'
+        }
+    }
+}
+</script>
+
+<style scoped>
+h2 {
+    color: #42b983;
+}
+</style>
